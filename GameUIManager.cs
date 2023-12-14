fb6f36e03bc7855537ba3ce2715f4457c7944aad
Saad <78092430+one-loop@users.noreply.github.com>
2023-12-15 00:38:48 +0530

    Added comments

--- GameUIManager.cs
+++ GameUIManager.cs
@@ -9,12 +9,7 @@ public class GameUIManager : MonoBehaviour
     public GameObject Credits;
     public GameObject Settings;
     public GameObject StartMenu;
-    public Material[] Themes;
-
-
-    private bool isMainMenuVisible = true; // Track the visibility of the main menu
-
-
+    public Material[] Themes; // array of material objects representing themes
 
     // Custom Function Start the whole game, quit the game
     public void StartGame() 
@@ -22,55 +17,28 @@ public class GameUIManager : MonoBehaviour
         // MainMenu.SetActive(false);
         Settings.SetActive(false);
         Credits.SetActive(false);
-        StartMenu.SetActive(true);
+        StartMenu.SetActive(true); // Show start menu when start button pressed
     }
 
+    // Quit Game
     public void QuitGame() 
     {
         Application.Quit();
     }
 
+    // Toggle Credits Panel UI visibility
     public void DisplayCredits()
     {
         Credits.SetActive(!Credits.activeSelf);
     }
 
-    public void DisplayMenu()
-    {
-        isMainMenuVisible = !isMainMenuVisible; // Toggle the visibility state
-
-        // if (MainMenu != null)
-        // {
-        //     if (isMainMenuVisible)
-        //     {
-        //         // Move the main menu back to its original position or keep it visible
-        //         // MainMenu.transform.position = new Vector3(0.09f, 0.922f, 0.814f); // Adjust this position
-        //         // mainMenu.transform.Rotate(new Vector3(10f, 37f, 0f));
-        //         
-        //     }
-        //     else
-        //     {
-        //         // Move the main menu to an off-screen location (e.g., far away from the camera)
-        //         // MainMenu.transform.position = new Vector3(0.525f, 1f, 0f); // Example off-screen position
-        //         // mainMenu.transform.Rotate(new Vector3(10f, 37f, 0f));
-        //         MainMenu.transform.rotation = new
-        //         Settings.SetActive(isMainMenuVisible); // Additional actions for other UI elements if needed
-        //         Credits.SetActive(isMainMenuVisible);
-        //     }
-        // }
-    }
-
+    // Toggle Settings Panel UI Visibility
     public void DisplaySettings()
     {
         Settings.SetActive(!Settings.activeSelf);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    // Set theme based on index passed
     public void SetTheme(int themeNumber)
     {
         RenderSettings.skybox = Themes[themeNumber - 1];
@@ -78,47 +46,12 @@ public class GameUIManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {    
+        // For debugging, show start menu when enter pressed
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            DisplayMenu();
             StartMenu.SetActive(true);
             // StartMenu.transform.position = new Vector3(0.09f, 0.922f, 0.814f);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape) || OVRInput.GetDown(OVRInput.Button.One))
-        {
-            DisplayMenu();
-        }
-
-        if (Input.GetKeyDown(KeyCode.C) && isMainMenuVisible)
-        {
-            DisplayCredits();
-        }
-
-        if (Input.GetKeyDown(KeyCode.S) && isMainMenuVisible)
-        {
-            DisplaySettings();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            SetTheme(1);
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            SetTheme(2);
-        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            SetTheme(3);
-        } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
-            SetTheme(4);
-        } else if (Input.GetKeyDown(KeyCode.Alpha5)) {
-            SetTheme(5);
-        } else if (Input.GetKeyDown(KeyCode.Alpha6)) {
-            SetTheme(6);
-        } else if (Input.GetKeyDown(KeyCode.Alpha7)) {
-            SetTheme(7);
-        } else if (Input.GetKeyDown(KeyCode.Alpha8)) {
-            SetTheme(8);
-        } else if (Input.GetKeyDown(KeyCode.Alpha9)) {
-            SetTheme(9);
         }
     }
 }
